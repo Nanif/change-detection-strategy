0fadf6a38e971c93456cd1bf5c6c53039fe36db9
neomi2152@gmail.com <neomi2152@gmail.com>
2023-12-24 03:17:34 +0200

    problem 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <div class="app-container">
+    	<button (click)="clickMe()">click me!</button>
+      <app-child-component></app-child-component>
+    </div>`
 })
 export class AppComponent {
   title = 'advanced-angular-change-detection';
+  clickMe() { }
 }
